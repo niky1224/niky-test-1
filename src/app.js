87c3e38642b7b1fc52efc420339c4b8d6730805d
niky123 <774034667@qq.com>
2018-08-03 15:09:01 +0800

    加入 parcel 打包

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import Vue from 'vue'
+import Button from "./button"
+Vue.component("g-buttton",Button)
+new Vue({
+    el:'#app',
+})
---
+++ src/button.vue
@@ -0,0 +1,25 @@
+<template>
+    <button class="g-button">hellow</button>
+</template>
+
+<script>
+    export default {
+    }
+</script>
+
+<style lang="scss" scoped>
+    .g-button{
+        font-size: var(--font-size);
+        height: var(--button-height);
+        padding: 0 1em;
+        border-radius:var(--button-radius) ;
+        border: 1px solid var(--border-color);
+        background: var(--button-bg);
+    }
+    .g-button:hover{
+        border-color:var(--border-hover-color) ;
+    }
+    .g-button:active{
+        background: var(--button-active-bg);
+    }
+</style>
