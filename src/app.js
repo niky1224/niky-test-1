f82fde4fe4ad33ab662dfbe37655bbc145d50b3c
niky123 <774034667@qq.com>
2018-08-09 10:23:29 +0800

    提交单元测试

--- src/app.js
+++ src/app.js
@@ -10,3 +10,52 @@ new Vue({
         loading2:false
     }
 })
+import chai from "chai"
+const expect=chai.expect;
+{
+    const constructor=Vue.extend(Button);
+    const vm=new constructor({
+        propsData:{
+            icon:'setting'
+        }
+    })
+    vm.$mount('#test')
+    let useelement=vm.$el.querySelector("use")
+    let href=useelement.getAttribute("xlink:href")
+    expect(href).to.eq("#my-setting")
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'settings',
+            loading: true
+        }
+    })
+    vm.$mount()
+    let useElement = vm.$el.querySelector('use')
+    let href = useElement.getAttribute('xlink:href')
+    expect(href).to.eq('#my-loading')
+    vm.$el.remove()
+    vm.$destroy()
+}
+
+{
+    const div = document.createElement('div')
+    document.body.appendChild(div)
+    const Constructor = Vue.extend(Button)
+    const vm = new Constructor({
+        propsData: {
+            icon: 'setting'
+        }
+    })
+    vm.$mount(div)
+    let svg = vm.$el.querySelector('svg')
+    let {order} = window.getComputedStyle(svg)
+    expect(order).to.eq('1')
+   vm.$el.remove()
+    vm.$destroy()
+}
